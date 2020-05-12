ebcf805ab15b92cc2da6c74fdc63b0ab5e6423d1
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 18:16:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,12 @@ require 'pry'
 def load_library
   # code goes here
   emoticons = YAML.load_file('emoticons.yml')
-  binding.pry
-  emoticons
+  emoticons.reduce({}) do |memo, (k, v)|
+    
+  end
+    
+  end
+
 end
 
 def get_japanese_emoticon

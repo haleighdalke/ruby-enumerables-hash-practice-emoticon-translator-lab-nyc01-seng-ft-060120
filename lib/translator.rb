e8224212e3edf70c70643c48811fe42c219eba29
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 19:18:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,6 @@ require 'pry'
 
 def load_library(file_name)
   # code goes here
-  emoticons = file_name.load_file
-  emoticons.reduce({}) do |memo, (k, v)|
-    v.map { |e|
-      memo[k] = {
-        :english => v[0],
-        :japanese => v[1]
-      }
-    }
-    memo
-  end
 end
 
 def get_japanese_emoticon
